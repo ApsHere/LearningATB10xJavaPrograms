c8e73fba2e5d7cad31f8232e213ead56ebff604e
Aprajita Archy <aprajita.archy@telus.com>
2025-01-08 15:38:36 +0530

    adding just 7th assignment

---
+++ src/fifthjan24/Lab007_PrintlnVSPrint.java
@@ -0,0 +1,9 @@
+package fifthjan24;
+
+public class Lab007_PrintlnVSPrint {
+    public static void main(String[] args){
+        System.out.println("this is for println"); //this will add new line after printing
+        System.out.print("this is for print");//This will not add new line so no new line between this statement and processmessage
+    //If after print any printing statement was there then it would have been printed in same line along with this print statement
+    }
+}
