2741b423f37e8bd87420bf63e77263dcbcfaeea5
Aprajita Archy <aprajita.archy@telus.com>
2025-01-30 16:51:37 +0530

    Updating 19th assignment

---
+++ src/EighteenthJan/AllOperators.java
@@ -0,0 +1,27 @@
+package EighteenthJan;
+
+import java.security.KeyStore;
+
+public class AllOperators {
+    public static void main(String[] args)
+    {
+        int A=10;
+        int B=20;
+        System.out.println(A+B);
+        System.out.println(A/B);
+       // boolean C = true;
+        boolean C= A>B;
+       // System.out.println( !(A>B));
+        System.out.println( !(C));
+      String first_name= "Raj";
+      String sec_name="Priya";
+      System.out.println(first_name+sec_name);
+      char X='A';
+       char Y='a';
+       System.out.println(X+Y);
+        System.out.println(X);
+        System.out.println(Y);
+        System.out.println(!(X>Y) && (X>Y));
+        System.out.println(!(X>Y) && (X<Y));
+    }
+}
